c547ef73505dcc7feeeec7c6f8a2a1e42a5528c3
zubair bin ferdous <zubairbinf@gmail.com>
2021-01-05 04:15:35 +0600

    solid city

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.slider-data').slick({
+        slidesToShow:5,
+        autoplay: true,
+        autoplaySpeed: 300,
+        arrows:false,
+    });
+
+
+  });
